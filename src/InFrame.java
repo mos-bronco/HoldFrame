89ff4d79a3b299bd32105faeecc9e4688d5a630d
mos-bronco <DanDuran@durantechnology.com>
2022-12-19 07:35:59 -0500

    Working Start Frame That is Green With Ts Icon

--- src/InFrame.java
+++ src/InFrame.java
@@ -1,21 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class InFrame extends JFrame{
 
 
 
-    void hFrame(){
+    public InFrame(){
 
         JFrame holdFrame = new JFrame();
         holdFrame.setTitle("title");
         holdFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         holdFrame.setLayout(null);
         holdFrame.setResizable(true);
-        holdFrame.setSize(400,400);
+
         holdFrame.setVisible(true);
+        holdFrame.setSize(400,400);
+        ImageIcon image = new ImageIcon("TsIcon.png");
+        holdFrame.setIconImage(image.getImage());
+        holdFrame.getContentPane().setBackground(Color.green);
+
+
+
+
+        //holdFrame.setBackground(Color.red);
+
 
 
     }
+
 
 
 }
