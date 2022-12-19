9107e90def6c5ae0765136b000ffc6875834f0f6
mos-bronco <DanDuran@durantechnology.com>
2022-12-19 07:20:52 -0500

    new

---
+++ src/InFrame.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class InFrame extends JFrame{
+
+
+
+    void hFrame(){
+
+        JFrame holdFrame = new JFrame();
+        holdFrame.setTitle("title");
+        holdFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        holdFrame.setLayout(null);
+        holdFrame.setResizable(true);
+        holdFrame.setSize(400,400);
+        holdFrame.setVisible(true);
+
+
+    }
+
+
+}
