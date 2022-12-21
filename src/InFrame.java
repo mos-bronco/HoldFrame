e080928dcf242ed0c81b966f4d76d2f45ccb336a
mos-bronco <DanDuran@durantechnology.com>
2022-12-21 13:52:52 -0500

    Complete probably

--- src/InFrame.java
+++ src/InFrame.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static java.lang.System.exit;
 import static java.lang.System.out;
 
 public class InFrame extends JFrame implements ActionListener {
@@ -54,10 +55,11 @@ public class InFrame extends JFrame implements ActionListener {
     JPanel Batchpanel;
 
     JButton Submit;
+    JButton Back;
     JButton Print;
     JButton Clear;
 
-    JTextField Printfield;
+
     JLabel Printlabel;
     JPanel Printpanel;
    static  String subFirst = "";
@@ -65,9 +67,9 @@ public class InFrame extends JFrame implements ActionListener {
 
     static JMenuBar Menubar;
     static JMenu Helpmenu;
+    static JMenu Filemenu;
     static JMenuItem help, Contactsupport, m3;
-
-
+    static JMenuItem filemenuitem;
     public void Charactercounter(String Comment) {
 
     int i = 0;
@@ -117,33 +119,47 @@ public class InFrame extends JFrame implements ActionListener {
         //System.out.println(subFirst);
         //System.out.println(subSecond);
     }
-public void Menu(){
+    public void Menu(){
     // create a menubar
     Menubar = new JMenuBar();
 
     // create a menu
     Helpmenu = new JMenu("Help");
+    Filemenu = new JMenu("File");
+        filemenuitem= new JMenuItem(new AbstractAction("Exit") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                System.exit(0);
+
+
+
+            }
+        });
 
     // create menuitems
     help = new JMenuItem(new AbstractAction("Help") {
         @Override
         public void actionPerformed(ActionEvent e) {
             JOptionPane.showMessageDialog(new JFrame(), """
-                    Press Submit after entering the desired Data for the listed fields. Any field can be left blank.
-                    After clicking Submit enter the quantity to print. 
-                    This must be a number and can not include any special characters or letters.
-                     
-                    """);
+                Step 1: Enter the desired information and press submit
+                Step 2: After pressing submit a pop up will open to enter the number of labels to print
+                Step 3: Press ok
+                Step 4: The fields will not be editable while in this window
+                Step 5: Press back or clear to unlock the fields
+                Step 6: Press print to confirm the print job and send the labels.""");
+
+
 
         }
     });
     Contactsupport = new JMenuItem("Contact Support");
     //m3 = new JMenuItem("MenuItem3");
-
+    Filemenu.add(filemenuitem);
     Helpmenu.add(help);
-    Helpmenu.add(Contactsupport);
+    //Helpmenu.add(Contactsupport);
     //x.add(m3);
-
+    Menubar.add(Filemenu);
     Menubar.add(Helpmenu);
 
     Helpmenu.addActionListener(this);
@@ -153,110 +169,69 @@ public void Menu(){
 
     public void textfields(){
         Defect = new JTextField();
-        Defect.setPreferredSize(new Dimension(100,25));
+        Defect.setPreferredSize(new Dimension(150,25));
         Part = new JTextField();
-        Part.setPreferredSize(new Dimension(100,25));
+        Part.setPreferredSize(new Dimension(150,25));
         Date = new JTextField();
-        Date.setPreferredSize(new Dimension(100,25));
+        Date.setPreferredSize(new Dimension(150,25));
         Requiredaction = new JTextField();
-        Requiredaction.setPreferredSize(new Dimension(100,25));
+        Requiredaction.setPreferredSize(new Dimension(350,25));
         Quantity = new JTextField();
-        Quantity.setPreferredSize(new Dimension(100,25));
+        Quantity.setPreferredSize(new Dimension(150,25));
         Inspector = new JTextField();
-        Inspector.setPreferredSize(new Dimension(100,25));
+        Inspector.setPreferredSize(new Dimension(150,25));
         Batch = new JTextField();
-        Batch.setPreferredSize(new Dimension(100,25));
-
-
+        Batch.setPreferredSize(new Dimension(150,25));
 
         Leftpanel = new JPanel();
 
-
-
-
         Defectpanel = new JPanel();
-        Defectpanel.setPreferredSize(new Dimension(125,50));
+        Defectpanel.setPreferredSize(new Dimension(150,50));
         Defectlabel = new JLabel("Defect");
         Defectlabel.setVerticalTextPosition(Defect.TOP);
         Defectlabel.setHorizontalTextPosition(Defect.CENTER);
         Defectlabel.setHorizontalAlignment(Defect.CENTER);
         Defectlabel.setVerticalAlignment(Defect.CENTER);
 
-
         Partpanel = new JPanel();
-        Partpanel.setPreferredSize(new Dimension(125,50));
+        Partpanel.setPreferredSize(new Dimension(150,50));
         Partlabel = new JLabel("Part Number");
         Partlabel.setHorizontalTextPosition(Part.LEFT);
 
         Datepanel = new JPanel();
-        Datepanel.setPreferredSize(new Dimension(125,50));
+        Datepanel.setPreferredSize(new Dimension(150,50));
         Datelabel = new JLabel("Date");
-
 
         Requiredpanel = new JPanel();
         Requiredlabel = new JLabel("Required Action");
-        Requiredpanel.setPreferredSize(new Dimension(125,50));
+        Requiredpanel.setPreferredSize(new Dimension(350,50));
         Requiredlabel.setVerticalTextPosition(Defect.TOP);
         Requiredlabel.setHorizontalTextPosition(Defect.CENTER);
 
         Quantitypanel = new JPanel();
-        Quantitypanel.setPreferredSize(new Dimension(125,50));
+        Quantitypanel.setPreferredSize(new Dimension(150,50));
         Quantitylabel = new JLabel("Quantity");
 
-
-
         Batchpanel = new JPanel();
-        Batchpanel.setPreferredSize(new Dimension(125,50));
+        Batchpanel.setPreferredSize(new Dimension(150,50));
         //Batchpanel.setBackground(Color.ORANGE);
         Batchlabel = new JLabel("Batch");
 
-
         Inspectorpanel = new JPanel();
-        Inspectorpanel.setPreferredSize(new Dimension(125,50));
+        Inspectorpanel.setPreferredSize(new Dimension(150,50));
         Inspectorlabel = new JLabel("Inspector");
 
         Printpanel = new JPanel();
-        //Printpanel.setPreferredSize(new Dimension(50,50));
-
-        //Printlabel.setHorizontalTextPosition(Part.LEFT);
-
     }
-
     public void Rightpaneladds(){
 
-
+        Rightpanel.add(Submit);
+        Rightpanel.add(Clear,BorderLayout.EAST);
 
     }
-    public InFrame(){
-        Menu();
 
-        Printlabel = new JLabel(String.valueOf(Pnumber));
+    public void Leftpaneladds(){
 
-        JFrame holdFrame = new JFrame();
-        holdFrame.setTitle("TRMD HOLD TAG PRINTER 2.0.0");
-        holdFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        holdFrame.setLayout(new BorderLayout());
-        holdFrame.setResizable(true);
-        holdFrame.setJMenuBar(Menubar);
-
-
-        holdFrame.setSize(400,400);
-        ImageIcon image = new ImageIcon("TsIcon.png");
-        holdFrame.setIconImage(image.getImage());
-        holdFrame.getContentPane().setBackground(Color.green);
-
-
-        textfields();
-
-
-        Border blackline = BorderFactory.createLineBorder(Color.black);
-        Rightpanel = new JPanel();
-        Submit = new JButton("Submit");
-        Submit.addActionListener(this);
-        Print= new JButton("Print");
-        Print.addActionListener(this);
-        Clear= new JButton("Clear");
-        Clear.addActionListener(this);
         Defectpanel.add(Defectlabel);
         Defectpanel.add(Defect);
         Partpanel.add(Partlabel);
@@ -272,31 +247,56 @@ public void Menu(){
         Batchpanel.add(Batchlabel);
         Batchpanel.add(Batch);
 
-        Leftpanel.setLayout(new FlowLayout());
-        Leftpanel.setBorder(blackline);
-        //Rightpanel.setLayout(new FlowLayout());
+
         Leftpanel.add(Defectpanel);
-        Leftpanel.add(Requiredpanel);
+
         Leftpanel.add(Datepanel);
         Leftpanel.add(Quantitypanel);
         Leftpanel.add(Batchpanel);
         Leftpanel.add(Partpanel);
         Leftpanel.add(Inspectorpanel);
+        Leftpanel.add(Requiredpanel);
 
+    }
+    public InFrame(){
+        Menu();
+        Printlabel = new JLabel(String.valueOf(Pnumber));
+        JFrame holdFrame = new JFrame();
+        holdFrame.setTitle("TRMD HOLD TAG PRINTER 2.0.0");
+        holdFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        holdFrame.setLayout(new BorderLayout());
+        holdFrame.setResizable(true);
+        holdFrame.setJMenuBar(Menubar);
+
+        holdFrame.setSize(400,400);
+        ImageIcon image = new ImageIcon("TsIcon.png");
+        holdFrame.setIconImage(image.getImage());
+        holdFrame.getContentPane().setBackground(Color.green);
+
+        textfields();
+
+        Border blackline = BorderFactory.createLineBorder(Color.black);
+        Rightpanel = new JPanel();
+        Submit = new JButton("Submit");
+        Submit.addActionListener(this);
+        Print= new JButton("Print");
+        Print.addActionListener(this);
+        Clear= new JButton("Clear");
+        Clear.addActionListener(this);
+        Back= new JButton("Back");
+        Back.addActionListener(this);
+        Leftpanel.setLayout(new FlowLayout());
+        Leftpanel.setBorder(blackline);
         Rightpanel.setPreferredSize(new Dimension(125,50));
         Rightpanel.setLayout(new BorderLayout());
-        Rightpanel.add(Submit);
-
         holdFrame.add(Leftpanel,BorderLayout.CENTER);
-
         holdFrame.add(Rightpanel, BorderLayout.SOUTH);
-        //holdFrame.add(Submit);
         Rightpanel.setSize(200,600);
         Leftpanel.setSize(200,600);
         holdFrame.setLocationRelativeTo(null);
-        //holdFrame.pack();
+        Leftpaneladds();
+        Rightpaneladds();
         holdFrame.setVisible(true);
-        Rightpanel.add(Clear,BorderLayout.EAST);
     }
 
 
@@ -308,9 +308,6 @@ public void Menu(){
             JOptionPane.showMessageDialog(new JFrame(), "You goofed up");
 
         }
-
-
-
         if (e.getSource() == Submit) {
             Printlabel.setText(" ");
             Pnumber= 0;
@@ -322,33 +319,36 @@ public void Menu(){
             sInspector = (Inspector.getText());
             sBatch = (Batch.getText());
 
+
+
             try {
                 Charactercounter(sComment);
                 if (OVER.equals("true")) {
-                    JOptionPane.showMessageDialog(new JFrame(), "You have entered too many characters //n for the resolution");
+                    JOptionPane.showMessageDialog(new JFrame(), "You have entered too many characters for the required action");
                     OVER = "";
                     Requiredaction.setBackground(Color.YELLOW);
                     Requiredaction.setText("");
 
                     return;
                 } else {
-                    Printqty = JOptionPane.showInputDialog("Please enter quantity to print");
+                    Printqty = JOptionPane.showInputDialog("Please enter the quantity to print");
                     Requiredaction.setBackground(Color.WHITE);
                     Pnumber = Integer.parseInt(Printqty);
                     String copyW = "";
-                    if(Pnumber > 1)
+                    if(Pnumber > 1 || Pnumber ==0)
                         copyW = "copies";
                     else copyW = "copy";
                     Printlabel = new JLabel("Click print to confirm you would like to print " + Pnumber + " " +copyW);
                     Printpanel.add(Printlabel);
 
+                    Requiredaction.setEditable(false);
+                    Defect.setEditable(false);
+                    Date.setEditable(false);
+                    Quantity.setEditable(false);
+                    Batch.setEditable(false);
+                    Part.setEditable(false);
+                    Inspector.setEditable(false);
 
-                    out.println(sDefect);
-                    out.println(sPart);
-                    out.println(sDate);
-                    out.println(sComment);
-                    out.println(sQuantity);
-                    out.println(sInspector);
 
 
                     Rightpanel.add(Clear,BorderLayout.EAST);
@@ -357,7 +357,7 @@ public void Menu(){
 
                     Rightpanel.add(Printpanel,BorderLayout.NORTH);
                     Rightpanel.remove(Submit);
-                    Rightpanel.add(Submit,BorderLayout.WEST);
+                    Rightpanel.add(Back,BorderLayout.WEST);
                     Rightpanel.repaint();
                     Rightpanel.revalidate();
                 }
@@ -366,16 +366,48 @@ public void Menu(){
 
             }
         }
+        if (e.getSource() == Back) {
+            Requiredaction.setEditable(true);
+            Defect.setEditable(true);
+            Date.setEditable(true);
+            Quantity.setEditable(true);
+            Batch.setEditable(true);
+            Part.setEditable(true);
+            Inspector.setEditable(true);
+
+            Rightpanel.remove(Clear);
+            Rightpanel.remove(Print);
+            Rightpanel.remove(Printpanel);
+            Rightpanel.remove(Back);
+
+            Rightpanel.add(Submit);
+            Rightpanel.add(Clear,BorderLayout.EAST);
+            Rightpanel.repaint();
+            Rightpanel.revalidate();
+
+
+
+
+        }
+
+
         if (e.getSource() == Print){
             try {
                 Pnumber = Integer.parseInt(Printqty);
                 Tag.main();
                 JOptionPane.showMessageDialog(new JFrame(), "Print Job Sent!");
+                Requiredaction.setEditable(true);
+                Defect.setEditable(true);
+                Date.setEditable(true);
+                Quantity.setEditable(true);
+                Batch.setEditable(true);
+                Part.setEditable(true);
+                Inspector.setEditable(true);
                 Rightpanel.remove(Print);
                 Rightpanel.remove(Printpanel);
                 Rightpanel.remove(Submit);
-                Rightpanel.repaint();
-                Rightpanel.revalidate();
+                Rightpanel.remove(Back);
+                Rightpanel.add(Clear,BorderLayout.EAST);
                 Rightpanel.add(Submit);
                 Rightpanel.repaint();
                 Rightpanel.revalidate();
@@ -383,11 +415,17 @@ public void Menu(){
                 JOptionPane.showMessageDialog(new JFrame(), "You must only enter numbers.");
 
             }
+            //JOptionPane.showMessageDialog(new JFrame(),"Print Job Sent!");
     }
         if (e.getSource() == Clear){
-
+            Requiredaction.setEditable(true);
+            Defect.setEditable(true);
+            Date.setEditable(true);
+            Quantity.setEditable(true);
+            Batch.setEditable(true);
+            Part.setEditable(true);
+            Inspector.setEditable(true);
             Printlabel.setText(" ");
-
             Defect.setText("");
             Part.setText("");
             Date.setText("");
@@ -407,7 +445,6 @@ public void Menu(){
             Rightpanel.repaint();
             Rightpanel.revalidate();
         }
-
 
             out.println(sDefect);
             out.println(sPart);
